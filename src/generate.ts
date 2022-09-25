844f8bd6300121361f25f83408820793bced43ab
Niloy Sikdar <niloysikdar30@gmail.com>
2022-09-26 00:15:53 +0530

    feat(generate): add full support for ts file types
    Add full support to generate .ts files with types support from env
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/generate.ts
+++ src/generate.ts
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
 
+import { join, resolve } from 'path';
+import { homedir } from 'os';
+import { readFileSync, writeFileSync } from 'fs';
+import { esmAndTsDefault } from './templates';
+import { parseData } from './parse';
+
 type Args = { [key: string]: string | boolean };
 
-function _getArgs(): Args {
+type ArgsSchema = {
+  path?: string;
+  encoding?: BufferEncoding;
+  out?: string;
+  ts?: boolean;
+  js?: boolean;
+  esm?: boolean;
+  cjs?: boolean;
+  debug?: boolean;
+};
+
+function _getArgs(): ArgsSchema {
   const args: string[] = process.argv.slice(2);
 
   const initial: Args = {};
@@ -15,5 +32,51 @@ function _getArgs(): Args {
   }, initial);
 }
 
-const args = _getArgs();
-console.log(args.js);
+function _resolveHome(envPath: string) {
+  return envPath[0] === '~' ? join(homedir(), envPath.slice(1)) : envPath;
+}
+
+function _generate({
+  path,
+  encoding = 'utf8',
+  out,
+  ts,
+  js,
+  esm,
+  cjs,
+  debug = false,
+}: ArgsSchema) {
+  const dotenvPath = path ? _resolveHome(path) : resolve(process.cwd(), '.env');
+
+  const defaultOutFile = 'env.config.' + (ts ? 'ts' : 'js');
+
+  const outPath = out
+    ? _resolveHome(out)
+    : resolve(process.cwd(), defaultOutFile);
+
+  const envData = readFileSync(dotenvPath, { encoding });
+  const parsedData = parseData(envData);
+
+  let envConfig = '';
+  let finalConfig = '';
+
+  if (ts) {
+    for (const finalKey in parsedData) {
+      debug && console.log(`Generating for: ${finalKey}`);
+      envConfig =
+        envConfig + `  ${finalKey}: process.env.${finalKey} as string,\n`;
+    }
+
+    finalConfig = esmAndTsDefault + envConfig + '};\n';
+  } else if (js) {
+    console.log(esm, cjs);
+  }
+
+  writeFileSync(outPath, finalConfig);
+  debug &&
+    console.log(
+      `${out ? out : defaultOutFile} has been generated successfully`,
+    );
+}
+
+_generate(_getArgs());
---
+++ src/templates.ts
@@ -0,0 +1,7 @@
+const esmAndTsDefault = `import { register } from 'neoenv';
+register();
+
+export const envConfig = {
+`;
+
+export { esmAndTsDefault };
