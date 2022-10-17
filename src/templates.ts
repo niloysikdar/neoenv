21ba82c3cbd9e1377f68b8fffac26832fb42b4b4
Niloy Sikdar <niloysikdar30@gmail.com>
2022-10-17 13:40:26 +0530

    feat(generate): immutable config object (#2)
    Use the Object.freeze method to make our config object immutable so that users can't modify the
    values
    
    Fixes #1
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- src/templates.ts
+++ src/templates.ts
@@ -6,28 +6,28 @@ type WithPath = {
 const esmAndTsDefault = `import { register } from 'neoenv';
 register();
 
-export const envConfig = {
+export const envConfig = Object.freeze({
 `;
 
 function esmAndTsDefaultWithPath({ path, encoding }: WithPath): string {
   return `import { register } from 'neoenv';
 register({ path: '${path}', encoding: '${encoding}' });
 
-export const envConfig = {
+export const envConfig = Object.freeze({
 `;
 }
 
 const cjsDefault = `const { register } = require('neoenv');
 register();
 
-exports.envConfig = {
+exports.envConfig = Object.freeze({
 `;
 
 function cjsDefaultWithPath({ path, encoding }: WithPath): string {
   return `const { register } = require('neoenv');
 register({ path: '${path}', encoding: '${encoding}' });
 
-exports.envConfig = {
+exports.envConfig = Object.freeze({
 `;
 }
 
