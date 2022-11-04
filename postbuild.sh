d9a17c08882e0190eeeaff59ba87f0a7a1090812
Niloy Sikdar <niloysikdar30@gmail.com>
2022-11-04 17:21:06 +0530

    feat(build): use esbuild instead of tsc
    Replace the tsc command with esbuild package to optimize our build process as well as overall
    package
    
    Fix #4
    
    Signed-off-by: Niloy Sikdar <niloysikdar30@gmail.com>

--- postbuild.sh
+++ postbuild.sh
@@ -3,24 +3,10 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Add type commonjs to the package.json file in the dist/cjs folder
-cat >dist/cjs/package.json <<!EOF
-{
-    "type": "commonjs"
-}
-!EOF
-
-# Add type module to the package.json file in the dist/esm folder
-cat >dist/esm/package.json <<!EOF
-{
-    "type": "module"
-}
-!EOF
-
 # Adding shebang to the top of the file
 sed -i '1i #!/usr/bin/env node' dist/cjs/generate.js
 
-# Removing the generate.js and generate.d.ts files from the dist/esm folder
-rm -rf dist/esm/generate.js dist/esm/generate.d.ts
+# Removing the generate.d.ts and templates.d.ts files from the dist/types folder
+rm -rf dist/types/generate.d.ts dist/types/templates.d.ts
 
 echo "Fixup done."
